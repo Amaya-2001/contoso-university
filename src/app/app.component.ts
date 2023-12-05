eb66e63a1cf1aa5f7b6ced749d1413a48d1bc70c
Amaya-2001 <112056360+Amaya-2001@users.noreply.github.com>
2023-12-05 15:00:00 +0530

    created the project and added the sign-up form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { UserSignupComponent } from './user-signup/user-signup.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, UserSignupComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
